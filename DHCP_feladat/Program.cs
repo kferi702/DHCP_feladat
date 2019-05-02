2df75408235d74d83caabf02286b4de234e20a72
Kovács Ferenc <kferi702@gmail.com>
2019-05-02 20:08:37 +0200

    Add project files.

---
+++ DHCP_feladat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DHCP_feladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
